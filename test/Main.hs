e3a2afa6563f97490612b5b02b03745bc18a4ab4
cyborggeneraal <alexvantilburg@outlook.com>
2024-03-18 17:49:47 +0100

    some initial test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
+import ApiCourses (appCourses, dummyCourses)
+import Test.Hspec
+import Test.Hspec.Wai
+import qualified Network.Wai.Handler.Warp as Warp
+import qualified Control.Concurrent as C
+import Control.Exception (bracket)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec spec
+
+spec :: Spec
+spec = do
+    businessLogicSpec
+
+withUserApp :: IO() -> IO ()
+withUserApp action =
+    bracket (C.forkIO $ Warp.run 9999 appCourses)
+      C.killThread
+      (const action)
+
+businessLogicSpec :: Spec
+businessLogicSpec =
+    around_ withUserApp $ do
+        with (pure $ appCourses) $ do
+            describe "GET /courses" $ do
+                it "should get dummy courses" $
+                  get "/courses" `shouldRespondWith` dummyCourses 
+    
+    -- withUserApp $ do
+    --     let createUser = client (Proxy :: Proxy CoursesAPI)
+    --     baseUrl <- runIO $ parseBaseUrl "http://localhost"
+    --     manager <- runIO $ newManager defaultManagerSettings
+    --     let clientEnv port = mkClientEnv manager undefined
+
+    --     describe "GET /courses" $ do
+    --         it "should get courses" $ \port -> do
+    --             result <- runClientM getAllCoursesHandler (clientEnv port)
+    --             result `shouldBe` (Right dummyCourses)
