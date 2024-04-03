b241dfc8cbfa6055598aa8e6883ca8b8b50e4da0
cyborggeneraal <alexvantilburg@outlook.com>
2024-04-03 16:32:33 +0200

    removed test that was unnessecary

--- src/test_Types.hs
+++
@@ -1,9 +0,0 @@
-import Test.Hspec
-import Types
-
-main :: IO ()
-main = hspec $ do
-  describe "Show instance for TimeSlot" $ do
-    it "returns the expected string representation" $ do
-      let timeSlot = TimeSlot 10 30
-      show timeSlot `shouldBe` "TimeSlot {hour = 10, minute = 30}"
